35cf8eeb1910c544f6a5991a87fd7fce5edca56e
shixinga <qqchenshixing@163.com>
2015-08-10 12:44:06 +0800

    13
    13

---
+++ 1ui/13Fragment/src/com/ui/fragment/Fragment02.java
@@ -0,0 +1,57 @@
+package com.ui.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment02 extends Fragment {
+
+	//返回的view对象会作为fragment01的内容显示在屏幕上
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment02, null);
+		return view;
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		System.out.println("02create");
+	}
+	
+	@Override
+	public void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+		System.out.println("02start");
+	}
+	
+	@Override
+	public void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		System.out.println("02resume");
+	}
+	
+	@Override
+	public void onPause() {
+		super.onPause();
+		System.out.println("022pause");
+	}
+	
+	@Override
+	public void onStop() {
+		super.onStop();
+		System.out.println("022stop");
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		System.out.println("022destroy");
+	}
+}
--- 1ui/13Fragment/src/com/ui/fragment/MainActivity.java
+++ 1ui/13Fragment/src/com/ui/fragment/MainActivity.java
@@ -1,34 +1,68 @@
 package com.ui.fragment;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
+	private Fragment03 fg3;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		fg3 = new Fragment03();
+    	//获取fragment管理器
+    	FragmentManager fm = getFragmentManager();
+    	//打开事务
+    	FragmentTransaction ft = fm.beginTransaction();
+    	//把内容显示至帧布局
+    	ft.replace(R.id.fl_right, fg3);
+    	//提交
+    	ft.commit();
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	public void fragment01OnClick(View view) {
+		//把fragment01的界面显示至帧布局中
+    	//创建fragment对象
+		Fragment01 fragment01 = new Fragment01();
+		//获取fragment管理器
+		FragmentManager fm = getFragmentManager();
+		//打开事务
+		FragmentTransaction ft = fm.beginTransaction();
+		//把内容显示至帧布局
+		ft.replace(R.id.fl_right, fragment01);
+		//提交
+		ft.commit();
 	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+	public void fragment02OnClick(View view) {
+		
+		//把fragment01的界面显示至帧布局中
+		//创建fragment对象
+		Fragment02 fragment02 = new Fragment02();
+		//获取fragment管理器
+		FragmentManager fm = getFragmentManager();
+		//打开事务
+		FragmentTransaction ft = fm.beginTransaction();
+		//把内容显示至帧布局
+		ft.replace(R.id.fl_right, fragment02);
+		//提交
+		ft.commit();
+	}
+	public void fragment03OnClick(View view) {
+		
+		//把fragment01的界面显示至帧布局中
+		//创建fragment对象
+		Fragment03 fragment03 = new Fragment03();
+		//获取fragment管理器
+		FragmentManager fm = getFragmentManager();
+		//打开事务
+		FragmentTransaction ft = fm.beginTransaction();
+		//把内容显示至帧布局
+		ft.replace(R.id.fl_right, fragment03);
+		//提交
+		ft.commit();
 	}
 }
